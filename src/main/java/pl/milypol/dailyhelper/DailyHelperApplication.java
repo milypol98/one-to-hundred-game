ed5ca4f076950b31485c980fe2d61e5037a85429
milypol98 <milypol98@gmail.com>
2022-10-12 18:33:57 +0200

    First commit

---
+++ src/main/java/pl/milypol/dailyhelper/DailyHelperApplication.java
@@ -0,0 +1,13 @@
+package pl.milypol.dailyhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyHelperApplication.class, args);
+    }
+
+}
