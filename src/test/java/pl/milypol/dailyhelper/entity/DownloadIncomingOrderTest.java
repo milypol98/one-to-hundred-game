b6ac1536a011f5add74c58223a759276a0ca989a
milypol98 <milypol98@gmail.com>
2022-10-18 19:55:58 +0200

    User and incoming class SQl code

---
+++ src/test/java/pl/milypol/dailyhelper/entity/DownloadIncomingOrderTest.java
@@ -0,0 +1,14 @@
+package pl.milypol.dailyhelper.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DownloadIncomingOrderTest {
+
+    @Test
+    public void testDownloadIncomingOrder() throws Exception {
+
+    }
+
+}
